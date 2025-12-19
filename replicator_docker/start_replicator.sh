00ed7660b68544b23009ad0927c4f62b6cba5626
Peter Gavriel <nullpetergavriel@gmail.com>
2025-12-19 13:43:25 -0500

    Made start_replicator easier to modify

--- replicator_docker/start_replicator.sh
+++ replicator_docker/start_replicator.sh
@@ -2,13 +2,33 @@
 # Step 2. Log in to the container registry: https://docs.nvidia.com/ngc/gpu-cloud/ngc-catalog-user-guide/index.html#logging-in-to-ngc-registry 
 # Step 3. Configure volume paths to mount folders for scripts, output, configs, models, and textures
 
+# Official Container: https://catalog.ngc.nvidia.com/orgs/nvidia/containers/ov-synthetic-data-generation?version=0.0.16-beta 
+
+# Modify local input/output directories
+INPUT_MODELS_DIR="/home/csrobot/Omniverse/Models"
+INPUT_MATERIALS_DIR="/home/csrobot/Omniverse/Materials/vMaterials_2"
+LOCAL_OUTPUT_DIR="/home/csrobot/Omniverse/SynthData"
+
+LOCAL_SCRIPT_DIR=$(realpath ./scripts)
+LOCAL_CONFIG_DIR=$(realpath ./config)
+
+echo "INPUT_MODELS_DIR:    $INPUT_MODELS_DIR"
+echo "INPUT_MATERIALS_DIR: $INPUT_MATERIALS_DIR"
+echo "LOCAL_OUTPUT_DIR:    $LOCAL_OUTPUT_DIR"
+echo "LOCAL_SCRIPT_DIR:    $LOCAL_SCRIPT_DIR"
+echo "LOCAL_CONFIG_DIR:    $LOCAL_CONFIG_DIR"
+sleep 1
+
 docker run --gpus all --entrypoint /bin/bash -it \
-    -v "/home/csrobot/synth_perception/replicator_docker/scripts:/home/ubuntu/scripts" \
-    -v "/home/csrobot/Omniverse/SynthData:/home/ubuntu/output" \
-    -v "/home/csrobot/synth_perception/replicator_docker/config:/home/ubuntu/config" \
-    -v "/home/csrobot/Omniverse/Models:/home/ubuntu/models" \
-    -v "/home/csrobot/Omniverse/Materials/vMaterials_2:/home/ubuntu/materials" \
-    ov-synthetic-data-generation-startup:v1
+    -v "$LOCAL_SCRIPT_DIR:/home/ubuntu/scripts" \
+    -v "$LOCAL_OUTPUT_DIR:/home/ubuntu/output" \
+    -v "$LOCAL_CONFIG_DIR:/home/ubuntu/config" \
+    -v "$INPUT_MODELS_DIR:/home/ubuntu/models" \
+    -v "$INPUT_MATERIALS_DIR:/home/ubuntu/materials" \
+    nvcr.io/nvidia/ov-synthetic-data-generation:0.0.16-beta
 
 # Original image name    
 # nvcr.io/nvidia/ov-synthetic-data-generation:0.0.16-beta
+
+# Custom image name (after following "Accelerating Start up time" instructions)
+# ov-synthetic-data-generation-startup:v1
